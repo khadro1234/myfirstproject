858d9da0334d0fe01d583cbbdc81181eee1824b0
khadro1234 <kadramohamedabdirhman@gmail.com>
2025-06-28 18:13:51 +0300

    initial commit

--- myfirstproject/Program.cs
+++ myfirstproject/Program.cs
@@ -1,5 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-string name = "Khadro Mohamed Abdirahman";
-Console.WriteLine(name);
+﻿
+    
+    String name = "Idil Mahad Yusuf"; Add commentMore action
+            Console.WriteLine("Name: " + name);
+string mothername = "Xamdi Ali";
+Console.WriteLine("Momother'Name: " + mothername);
+string dop = "2000-05-16";
+Console.WriteLine("Date Of Birth: " + dop);
+string pob = "Galkio";
+Console.WriteLine("Place Of Pirt: " + pob);
+string address = "Garsoor";
+Console.WriteLine("Address: " + address);
+int telphone = 097765567;
+Console.WriteLine("Telphone: " + telphone);
+string sex = "Female";
+Console.WriteLine("Sex: " + sex);
+string status = "Single";
+Console.WriteLine("Status: " + status);
+Add comment
