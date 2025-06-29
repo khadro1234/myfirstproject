2656a96551e01b9fee95d2e6543e9a3dba6f30f0
khadro1234 <kadramohamedabdirhman@gmail.com>
2025-06-29 20:18:06 +0300

    Lesson Successful

--- myfirstproject/Program.cs
+++ myfirstproject/Program.cs
@@ -1,19 +1,19 @@
 ﻿
     
-    String name = "Idil Mahad Yusuf"; Add commentMore action
+    String name = "Khadro mohamed abdirahman";// Add commentMore action
             Console.WriteLine("Name: " + name);
-string mothername = "Xamdi Ali";
+string mothername = "Nasra yusuf";
 Console.WriteLine("Momother'Name: " + mothername);
 string dop = "2000-05-16";
 Console.WriteLine("Date Of Birth: " + dop);
 string pob = "Galkio";
 Console.WriteLine("Place Of Pirt: " + pob);
-string address = "Garsoor";
+string address = "Israac";
 Console.WriteLine("Address: " + address);
-int telphone = 097765567;
+int telphone = 0907221799;
 Console.WriteLine("Telphone: " + telphone);
 string sex = "Female";
 Console.WriteLine("Sex: " + sex);
 string status = "Single";
 Console.WriteLine("Status: " + status);
-Add comment
+//Add comment
